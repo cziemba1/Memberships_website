3dbe230010bb980612537e1bbb3fc1027f180db4
Claudia Ziemba <claudia.ziemba@gmail.com>
2020-08-31 23:25:01 -0300

    FIrst commit

---
+++ Membership_website/Membership_website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Membership_website.Startup))]
+namespace Membership_website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
